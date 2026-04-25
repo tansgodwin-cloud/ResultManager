9512d789605e2dcbb72d6dd595a4298be2588ed9
Tans Tuân Vũ <tansgodwin@gmail.com>
2026-04-25 14:10:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
-import subprocess
-import sys
+import customtkinter as ctk
+from Login import LoginFrame
+from register import RegisterFrame
+from Home import HomeFrame
+from admin import AdminFrame
 
-def main():
-    # Khởi chạy file đăng nhập
-    subprocess.Popen([sys.executable, "login.py"])
+
+class MainApplication(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Phần mềm Quản lý Tài khoản")
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+        self.container = ctk.CTkFrame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        #kích cỡ của Giao diện admin
+        self.kich_thuoc_cua_so = {
+            "LoginFrame": "400x380",
+            "RegisterFrame": "450x650",
+            "HomeFrame": "1100x650",
+            "AdminFrame": "800x600"
+        }
+
+        # import bài khác
+        for F in (LoginFrame, RegisterFrame, HomeFrame, AdminFrame):
+            page_name = F.__name__
+            frame = F(parent=self.container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("LoginFrame")
+
+    def show_frame(self, page_name):
+        frame = self.frames.get(page_name)
+        if frame:
+            size_moi = self.kich_thuoc_cua_so.get(page_name, "800x600")
+            self.geometry(size_moi)
+            frame.tkraise()
+        else:
+            print(f"Lỗi: Không tìm thấy giao diện {page_name}")
+
 
 if __name__ == "__main__":
-    main()
+    app = MainApplication()
+    app.mainloop()
